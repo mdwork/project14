acb079f9cf35ac940ae1bc8c377b82b948d9dacf
Zhe1ka <zheka.dzecina@gmail.com>
2014-12-15 23:41:54 +0200

    Zhe1ka

---
+++ js/common.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.link-toggle-js').on('click', function(e){
+        e.preventDefault();
+        $(this).parent().find('.sub-menu-js').slideToggle().end().end().toggleClass('active');
+    });
+});
