ca2a8870cbf82db8c0f85d69347624964ece772e
mdwork <mazdze@gmail.com>
2014-12-17 11:04:57 +0200

    Zhe1ka

--- js/common.js
+++ js/common.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
         $(this).parent().find('.sub-menu-js').slideToggle().end().end().toggleClass('active');
     });
 
+    $('.minical_day_12_16_2014').addClass('bg_yellow');
+    $('.minical_day_12_10_2014').addClass('bg_yellow');
+
     $('.bg_yellow').on('click', function(){
         $('.wrap-popup').css({'left': $(this).offset().left,
             'top': $(this).offset().top});
