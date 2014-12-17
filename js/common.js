6fc344592ce3cbb8918d0777dc820df5f4fa2cf6
mdwork <mazdze@gmail.com>
2014-12-17 15:16:53 +0200

    Zhe1ka

--- js/common.js
+++ js/common.js
@@ -1,14 +1,41 @@
 $(document).ready(function() {
+    $("form.inline :text").minical({
+        inline: true
+    });
+
     $('.link-toggle-js').on('click', function(e){
         e.preventDefault();
         $(this).parent().find('.sub-menu-js').slideToggle().end().end().toggleClass('active');
     });
 
-    $('.minical_day_12_16_2014').addClass('bg_yellow');
-    $('.minical_day_12_10_2014').addClass('bg_yellow');
+    function dateWorks() {
+        $('.minical_day_12_16_2014').addClass('bg_yellow');
+        $('.minical_day_12_10_2014').addClass('bg_yellow');
+        $('.minical_day_12_9_2014').addClass('bg_green');
+        $('.minical_day_12_8_2014').addClass('bg_red');
+    }
+    dateWorks();
 
-    $('.bg_yellow').on('click', function(){
-        $('.wrap-popup').css({'left': $(this).offset().left,
-            'top': $(this).offset().top});
+    $('.minical_prev').on('click', function(){
+        $('.minical_day_12_16_2014').addClass('bg_yellow');
+        $('.minical_day_12_10_2014').addClass('bg_yellow');
+        $('.minical_day_12_9_2014').addClass('bg_green');
+        $('.minical_day_12_8_2014').addClass('bg_red');
+
+        dateWorks();
+    });
+
+    var wrapPopupCalendar = $('.wrap-popup');
+    $('.bg_yellow, .bg_green').on('click', function(){
+        wrapPopupCalendar.css({'left': $(this).offset().left + 35,
+            'top': $(this).offset().top}).addClass('display');
+
+        $('body').on('click', function(){
+            wrapPopupCalendar.removeClass('display');
+
+            wrapPopupCalendar.on('click', function(e) {
+                e.stopPropagation();
+            });
+        })
     });
 });
